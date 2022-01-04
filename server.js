7d120363e1b16bb244515bd09187e05e914ef834
Rehab Ayman <rehabayman.fcis@gmail.com>
2022-01-04 16:56:42 +0200

    update env.example and adding dotenv-expand

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv-expand')(require('dotenv').config());
 const PORT = process.env.PORT;
 const db = require('./database/db');
 const {authRouter} = require('./routes/authRouter');
